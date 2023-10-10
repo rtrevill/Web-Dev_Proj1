ae606ff6f7eacb9129159034284889c5c9250828
Jason Kenway <141990356+SpiralRezz@users.noreply.github.com>
2023-10-10 19:33:51 +1100

    Test 2 folder

---
+++ test/script.js
@@ -0,0 +1,32 @@
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const resultsList = document.querySelector('#results');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchRecipes();
+})
+
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=54c5251a&app_key=82d487d6c54980dd9d7e622018bf449b`);
+    const data = await response.json();
+    displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+    let html = '';
+    recipes.forEach((recipe) => {
+        html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div> 
+        `
+    })
+    resultsList.innerHTML = html;
+}
